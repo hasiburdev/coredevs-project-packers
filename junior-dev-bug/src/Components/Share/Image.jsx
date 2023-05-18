a1e9e83cafb737e70c27d4110a1b9e4c0cb27b3e
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-05-19 00:15:39 +0600

    Fix: Image works with image data also.

--- junior-dev-bug/src/Components/Share/Image.jsx
+++ junior-dev-bug/src/Components/Share/Image.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import { DefaultImg } from '../../Assets';
+import React from "react";
+import { DefaultImg } from "../../Assets";
 
 export default function Image({ src, className }) {
   return (
-    <img src={src?.startsWith('https://') ? src : `${process.env.REACT_APP_SERVER_URL || src}`} onError={(e) => { e.target.src = DefaultImg; }} className={`${className}`} alt="images" />
+    <img
+      src={
+        src?.startsWith("https://") | src?.startsWith("data:image")
+          ? src
+          : `${process.env.REACT_APP_SERVER_URL + src}`
+      }
+      onError={(e) => {
+        e.target.src = DefaultImg;
+      }}
+      className={`${className}`}
+      alt="images"
+    />
   );
 }
